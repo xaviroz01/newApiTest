ae9712ecd668bd8d83f3080a9122486d7dbc1f0e
Passadee <passadee.ki@gmail.com>
2022-07-04 23:37:56 +0700

    getv2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.route('/').get( (req, res) => {
+app.route('').get( (req, res) => {
   res.send('Hello from Node.js RESTful API');
 });
 
