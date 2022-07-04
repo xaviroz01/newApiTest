f427a5ea310c54f280c0d700743aa0624ac9a330
Passadee <passadee.ki@gmail.com>
2022-07-04 23:35:58 +0700

    test2

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.route('').get( (req, res) => {
+app.route('/').get( (req, res) => {
   res.send('Hello from Node.js RESTful API');
 });
+
+app.route('/hoho').get( (req, res) => {
+    res.send('Hoho!');
+  });
+  
 
 app.listen(port, () => {
   console.log('server is running on port: ', port);
